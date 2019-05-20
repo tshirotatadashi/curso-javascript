59d5ae5728d207cea526d614b8247fa55f2c5ea4
tshirotatadashi <tshirotatadashi@gmail.com>
2019-05-20 08:29:28 -0400

    Final da Aula 2.3

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,59 @@
+var ConnectionFactory = (function () {
+
+  const stores = ['negociacoes']
+  const version = 2
+  const dbName = 'aluraframe'
+
+  var connection = null
+  var close = null
+
+  return class ConnectionFactory {
+
+    constructor () {
+      throw new Error('Você não pode instanciar uma ConnectionFactory')
+    }
+
+    static getConnection () {
+      return new Promise((resolve, reject) => {
+
+        let openRequest = window.indexedDB.open(dbName,version)
+
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createStores(e.target.result)
+        }
+
+        openRequest.onsuccess = e => {
+          if (!connection) {
+            connection = e.target.result
+            close = connection.close.bind(connection)
+            connection.close = function() {
+              throw new Error('Você não pode fechar diretamente a conexão')
+            }
+          }
+          resolve(connection)
+        }
+
+        openRequest.onerror = e => {
+          console.log(e.target.error)
+          reject(e.target.error.name)
+        }
+      })
+    }
+
+    static closeConnection () {
+      if (connection) {
+        close()
+        connection = null
+        close = null
+      }
+    }
+
+    static _createStores (connection) {
+      stores.map(store => {
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store)
+        connection.createObjectStore(store, {autoIncrement: true})
+      })
+    }
+  }
+})()
