089954d7b91f3498a7418500bf038e6487c1580b
tshirotatadashi <tshirotatadashi@gmail.com>
2019-05-21 15:15:58 -0400

    Final da Aula 5.3

--- client/js/app/services/HttpConnect.js
+++ client/js/app/services/HttpConnect.js
@@ -1,38 +1,23 @@
 class HttpConnect {
 
+  _handleErrors (res) {
+    if (!res.ok) throw new Error(res.statusText)
+    return res
+  }
+
   get (url) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest()
-      xhr.open('GET', url)
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText))
-          } else {
-            reject(xhr.responseText)
-          }
-        }
-      }
-      xhr.send()
-    })
+    return fetch(url)
+      .then(res => this._handleErrors(res))
+      .then(res => res.json())
   }
 
   post (url, payload, headers) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest()
-      xhr.open('POST', url, true)
-      xhr.setRequestHeader(...headers)
-      xhr.onreadystatechange = () => {
-          if (xhr.readyState == 4) {
-              if (xhr.status !== 200) {
-                resolve(JSON.parse(xhr.responseText))
-              } else {
-                reject(xhr.responseText)
-              }
-          }
-      }
-      xhr.send(JSON.stringify(payload))
+    return fetch(url, {
+      headers: {'Content-Type': 'application/json'},
+      method: 'POST',
+      body: JSON.stringify(payload)
     })
+    .then(res => this._handleErrors(res))
   }
 
 }
